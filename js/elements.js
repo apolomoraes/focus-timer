74967ff7e51f5b3c5d414be5925c98417b8fc5ba
Apolo Moraes <vlogsapolo@gmail.com>
2022-10-14 20:44:35 -0300

    refatoração

---
+++ js/elements.js
@@ -0,0 +1,49 @@
+const buttonNormalMode = document.querySelector('.sun')
+const buttonDarkMode = document.querySelector('.moon')
+
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonIncrease = document.querySelector('.increase')
+const buttonDecrease = document.querySelector('.decrease')
+const buttonForest = document.querySelector('.forest-audio')
+const buttonRain = document.querySelector('.rain-audio')
+const buttonStore = document.querySelector('.store-audio')
+const buttonFire = document.querySelector('.fire-audio')
+
+const svgForest = document.querySelector('.svg-forest')
+const svgRain = document.querySelector('.svg-rain')
+const svgStore = document.querySelector('.svg-store')
+const svgFire = document.querySelector('.svg-fire')
+
+const volForest = document.querySelector('#input-forest')
+const volRain = document.querySelector('#input-rain')
+const volStore = document.querySelector('#input-store')
+const volFire = document.querySelector('#input-fire')
+
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+export const elements = {
+  buttonNormalMode,
+  buttonDarkMode,
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonIncrease,
+  buttonDecrease,
+  buttonForest,
+  buttonRain,
+  buttonStore,
+  buttonFire,
+  svgForest,
+  svgRain,
+  svgStore,
+  svgFire,
+  volForest,
+  volRain,
+  volStore,
+  volFire,
+  minutesDisplay,
+  secondsDisplay
+}
--- js/index.js
+++ js/index.js
@@ -1,32 +1,31 @@
 import Sound from './sounds.js'
 import { Timer } from './timer.js'
 import { darkMode } from './theme.js'
+import { elements } from './elements.js'
 
-const buttonNormalMode = document.querySelector('.sun')
-const buttonDarkMode = document.querySelector('.moon')
-
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonIncrease = document.querySelector('.increase')
-const buttonDecrease = document.querySelector('.decrease')
-const buttonForest = document.querySelector('.forest-audio')
-const buttonRain = document.querySelector('.rain-audio')
-const buttonStore = document.querySelector('.store-audio')
-const buttonFire = document.querySelector('.fire-audio')
-
-const svgForest = document.querySelector('.svg-forest')
-const svgRain = document.querySelector('.svg-rain')
-const svgStore = document.querySelector('.svg-store')
-const svgFire = document.querySelector('.svg-fire')
-
-const volForest = document.querySelector('#input-forest')
-const volRain = document.querySelector('#input-rain')
-const volStore = document.querySelector('#input-store')
-const volFire = document.querySelector('#input-fire')
-
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
+const {
+  buttonNormalMode,
+  buttonDarkMode,
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonIncrease,
+  buttonDecrease,
+  buttonForest,
+  buttonRain,
+  buttonStore,
+  buttonFire,
+  svgForest,
+  svgRain,
+  svgStore,
+  svgFire,
+  volForest,
+  volRain,
+  volStore,
+  volFire,
+  minutesDisplay,
+  secondsDisplay
+} = elements
 
 const timer = Timer({
   minutesDisplay,
@@ -44,11 +43,6 @@ function resetControl() {
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
 }
-
-darkMode({
-  buttonDarkMode,
-  buttonNormalMode
-})
 
 /* Button Timer */
 
@@ -110,10 +104,9 @@ buttonFire.addEventListener('click', () => {
 })
 
 /*  Button Theme */
-
-buttonDarkMode.addEventListener('click', () => {
-  buttonDarkMode.classList.add('hide')
-  buttonNormalMode.classList.remove('hide')
+darkMode({
+  buttonDarkMode,
+  buttonNormalMode
 })
 
 /* Event Volume */
